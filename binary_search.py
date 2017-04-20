b4dea2f3806f9190ca6b823bc5bc996742351729
probuse <nprobuse@gmail.com>
2017-04-20 12:50:40 +0300

    Created binary_search.py

---
+++ binary_search.py
@@ -0,0 +1,8 @@
+class BinarySearch(list):
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        super(BinarySearch, self).__init__()
+        
+    def search(self, value_to_find):
+        pass
